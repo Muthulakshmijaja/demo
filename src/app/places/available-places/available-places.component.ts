1677dd3157e2799c56696ccfd165fe48cf18d532
Muthulakshmijaja <muthulakshmijacsi@gmail.com>
2024-09-25 14:02:27 +0530

    udemy project

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,46 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit{
+  places = signal<Place[] | undefined>(undefined);
+  isFetching=signal(false)
+  private httpClient=inject(HttpClient);
+  private destroyRef=inject(DestroyRef)
+
+  ngOnInit(){
+    this.isFetching.set(true);
+    const Subscription= this.httpClient
+    .get<{places: Place[]}>('http://localhost:3000/places')
+    .pipe(
+      map((resData)=>resData.places)
+    )
+    
+    .subscribe({
+      next:(places)=>{
+        console.log(places),
+        this.places.set(places)
+    },
+    complete: ()=> {
+      this.isFetching.set(false)
+
+    }
+    });
+
+    this.destroyRef.onDestroy(()=>{
+      Subscription.unsubscribe();
+    });
+
+  }
+}
